17e97f640384180bc68c080aa49fb2bfcf4a1ceb
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2024-11-22 21:26:27 -0300

    Add getter and setter methods to Discount struct for improved encapsulation

--- domain/entities/discount.go
+++ domain/entities/discount.go
@@ -37,3 +37,47 @@ func NewDiscount(
 		description:  description,
 	}
 }
+
+func (d *Discount) ID() uuid.UUID {
+	return d.id
+}
+
+func (d *Discount) RestaurantID() uuid.UUID {
+	return d.restaurantID
+}
+
+func (d *Discount) UserID() uuid.UUID {
+	return d.userID
+}
+
+func (d *Discount) Code() string {
+	return d.code
+}
+
+func (d *Discount) Percentage() int {
+	return d.percentage
+}
+
+func (d *Discount) StartDate() time.Time {
+	return d.startDate
+}
+
+func (d *Discount) EndDate() time.Time {
+	return d.endDate
+}
+
+func (d *Discount) Times() int {
+	return d.times
+}
+
+func (d *Discount) Description() string {
+	return d.description
+}
+
+func (d *Discount) SetID(id uuid.UUID) {
+	d.id = id
+}
+
+func (d *Discount) SetCode(code string) {
+	d.code = code
+}
