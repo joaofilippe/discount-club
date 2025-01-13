08cc72b350736ff22aa607938114a3d993905fb5
João Filippe Rossi Rodrigues <joaofilippe@outlook.com>
2025-01-13 19:04:28 -0300

    refactor: optimize discount creation logic and enhance request validation

--- app/domain/usecases/discount/create.usecase.go
+++ app/domain/usecases/discount/create.usecase.go
@@ -1,92 +1,90 @@
 package discountusecases
 
 import (
-    "errors"
-    "fmt"
-    "time"
+	"fmt"
+	"math/rand/v2"
+	"time"
 
-    "github.com/google/uuid"
-    "github.com/joaofilippe/discount-club/app/domain/entities"
-    discounterrors "github.com/joaofilippe/discount-club/common/myerrors/discount"
-    "github.com/joaofilippe/discount-club/common/rand"
+	discounterrors "github.com/joaofilippe/discount-club/app/common/myerrors/discount"
+	"github.com/joaofilippe/discount-club/app/domain/entities"
 )
 
 // DiscountRepository defines the interface for discount repository
 type DiscountRepository interface {
-    Save(discount *entities.Discount) error
+	Save(discount *entities.Discount) error
 }
 
 // CreateUseCase handles the creation of discounts
 type CreateUseCase struct {
-    DiscountRepo DiscountRepository
-    randSource   *rand.Rand
+	DiscountRepo DiscountRepository
+	randSource   *rand.Rand
 }
 
 // NewCreateUseCase creates a new instance of CreateUseCase
 func NewCreateUseCase(discountRepo DiscountRepository) (*CreateUseCase, error) {
-    if discountRepo == nil {
-        return nil, discounterrors.ErrNilDiscountRepo
-    }
+	if discountRepo == nil {
+		return nil, discounterrors.ErrNilDiscountRepo
+	}
 
-    pcg := rand.NewPCG(uint64(time.Now().Unix()), uint64(time.Now().Second()))
-    r := rand.New(pcg)
+	pcg := rand.NewPCG(uint64(time.Now().Unix()), uint64(time.Now().Second()))
+	r := rand.New(pcg)
 
-    return &CreateUseCase{
-        DiscountRepo: discountRepo,
-        randSource:   r,
-    }, nil
+	return &CreateUseCase{
+		DiscountRepo: discountRepo,
+		randSource:   r,
+	}, nil
 }
 
 // Execute processes the discount creation request
 func (uc *CreateUseCase) Execute(request *entities.Discount) (*entities.Discount, error) {
-    if request == nil {
-        return entities.NewEmptyDiscount(), discounterrors.ErrNoDiscountProvided
-    }
+	if request == nil {
+		return entities.NewEmptyDiscount(), discounterrors.ErrNoDiscountProvided
+	}
 
-    if err := uc.validatedRequest(request); err != nil {
-        return entities.NewEmptyDiscount(), err
-    }
+	if err := uc.validatedRequest(request); err != nil {
+		return entities.NewEmptyDiscount(), err
+	}
 
-    initializedDiscount := uc.prepareDiscount(request)
+	initializedDiscount := uc.prepareDiscount(request)
 
-    return initializedDiscount, uc.DiscountRepo.Save(initializedDiscount)
+	return initializedDiscount, uc.DiscountRepo.Save(initializedDiscount)
 }
 
 // prepareDiscount initializes a new discount with a generated code
 func (uc *CreateUseCase) prepareDiscount(discount *entities.Discount) *entities.Discount {
-    code := uc.generateCode(uc.randSource)
-    return discount.CopyWith(nil, nil, &code, nil, nil, nil, nil)
+	code := uc.generateCode(uc.randSource)
+	return discount.CopyWith(nil, nil, &code, nil, nil, nil, nil, nil)
 }
 
 // generateCode generates a random discount code
 func (uc *CreateUseCase) generateCode(r *rand.Rand) string {
-    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-    b := make([]byte, 6)
-    for i := range b {
-        b[i] = charset[r.IntN(len(charset))]
-    }
+	b := make([]byte, 6)
+	for i := range b {
+		b[i] = charset[r.IntN(len(charset))]
+	}
 
-    return string(b)
+	return string(b)
 }
 
 // validatedRequest validates the discount request
 func (uc *CreateUseCase) validatedRequest(discount *entities.Discount) error {
-    if discount.Code() != "" {
-        return discounterrors.ErrCodeProvidedOnCreate
-    }
+	if discount.Code() != "" {
+		return discounterrors.ErrCodeProvidedOnCreate
+	}
 
-    if discount.Percentage() <= 0 || discount.Percentage() > 100 {
-        return fmt.Errorf("invalid percentage: %d", discount.Percentage())
-    }
+	if discount.Percentage() <= 0 || discount.Percentage() > 100 {
+		return fmt.Errorf("invalid percentage: %d", discount.Percentage())
+	}
 
-    if discount.Times() <= 0 {
-        return fmt.Errorf("invalid times: %d", discount.Times())
-    }
+	if discount.Times() <= 0 {
+		return fmt.Errorf("invalid times: %d", discount.Times())
+	}
 
-    if discount.Description() == "" {
-        return fmt.Errorf("description is required")
-    }
+	if discount.Description() == "" {
+		return fmt.Errorf("description is required")
+	}
 
-    return nil
+	return nil
 }
